c2f95af362f8b6a6628df4140506892c5c47c977
xaxxoo <bnahmad83@gmail.com>
2026-07-05 16:08:13 +0100

    feat(vas): wire VasModule with TypeORM, VTPassModule and AuthModule

---
+++ src/vas/vas.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VasController } from './vas.controller';
+import { VasService } from './vas.service';
+import { Transaction } from '../database/entities/transaction.entity';
+import { LedgerEntry } from '../database/entities/ledger-entry.entity';
+import { Account } from '../database/entities/account.entity';
+import { VTPassModule } from '../providers/vtpass/vtpass.module';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Transaction, LedgerEntry, Account]),
+    VTPassModule,
+    AuthModule,
+  ],
+  controllers: [VasController],
+  providers: [VasService],
+})
+export class VasModule {}
