5dc8808f687ff0cdc13bb01f15157c2ffff492a6
xaxxoo <bnahmad83@gmail.com>
2026-07-05 15:51:32 +0100

    feat(accounts): wire AccountsModule with TypeORM, AnchorModule and AuthModule

---
+++ src/accounts/accounts.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountsController } from './accounts.controller';
+import { AccountsService } from './accounts.service';
+import { Account } from '../database/entities/account.entity';
+import { AnchorModule } from '../providers/anchor/anchor.module';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Account]),
+    AnchorModule,
+    AuthModule,
+  ],
+  controllers: [AccountsController],
+  providers: [AccountsService],
+  exports: [AccountsService],
+})
+export class AccountsModule {}
