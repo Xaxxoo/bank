a79ffd4e63438e4870e18cf9d95bd29a90334bdc
xaxxoo <bnahmad83@gmail.com>
2026-07-06 08:23:08 +0100

    feat: import WebhooksModule into AccountsModule
    Makes WebhooksService available for injection into AccountsService
    so account.created events can be delivered after account provisioning.

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -5,12 +5,14 @@ import { AccountsService } from './accounts.service';
 import { Account } from '../database/entities/account.entity';
 import { AnchorModule } from '../providers/anchor/anchor.module';
 import { AuthModule } from '../auth/auth.module';
+import { WebhooksModule } from '../webhooks/webhooks.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Account]),
     AnchorModule,
     AuthModule,
+    WebhooksModule,
   ],
   controllers: [AccountsController],
   providers: [AccountsService],
