1abd44c9a087802df11c21175e38653d257a8345
xaxxoo <bnahmad83@gmail.com>
2026-07-06 08:07:07 +0100

    feat: import WebhooksModule into VasModule
    Makes WebhooksService available for injection into VasService.

--- src/vas/vas.module.ts
+++ src/vas/vas.module.ts
@@ -7,12 +7,14 @@ import { LedgerEntry } from '../database/entities/ledger-entry.entity';
 import { Account } from '../database/entities/account.entity';
 import { VTPassModule } from '../providers/vtpass/vtpass.module';
 import { AuthModule } from '../auth/auth.module';
+import { WebhooksModule } from '../webhooks/webhooks.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Transaction, LedgerEntry, Account]),
     VTPassModule,
     AuthModule,
+    WebhooksModule,
   ],
   controllers: [VasController],
   providers: [VasService],
